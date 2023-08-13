8a1536e7afb493ec7d138cd824d6c40e083b3c4f
hadiadjd <celestialgeek21@gmail.com>
2023-08-13 19:31:30 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+const body = document.querySelector('body');
+body.appendChild(newHeader);
+document.getElementsByTagName("h1")[0].innerHTML="Orion is the champion";
